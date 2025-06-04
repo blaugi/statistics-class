8efbf54a42d368442cb4382ddc76c8d4d942cc8c
Matheus Girardi <matheusmgirardi@gmail.com>
2025-06-04 18:06:33 -0300

    feat: tde1 passo 3

---
+++ a18/target/debug/deps/a18-33d881d8cfaa9d45.d
@@ -0,0 +1,5 @@
+/home/blau/projects/statistics-class/a18/target/debug/deps/liba18-33d881d8cfaa9d45.rmeta: src/main.rs
+
+/home/blau/projects/statistics-class/a18/target/debug/deps/a18-33d881d8cfaa9d45.d: src/main.rs
+
+src/main.rs:
---
+++ a18/target/debug/deps/a18-6787ae83f756c9b6.d
@@ -0,0 +1,5 @@
+/home/blau/projects/statistics-class/a18/target/debug/deps/liba18-6787ae83f756c9b6.rmeta: src/main.rs
+
+/home/blau/projects/statistics-class/a18/target/debug/deps/a18-6787ae83f756c9b6.d: src/main.rs
+
+src/main.rs:
